8e8a14d847a0a1a4d52eb869abfcf151ba4bf135
Jaime Pericas <jaime.pericas@nike.com>
2020-01-07 14:15:35 -0800

    expressJs @types added and working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-const express = require('express');
+import { default as express } from 'express';
 
 const app = express();
 const PORT = 3000;
+
+app.use('/', (req, res): void => {
+  res.send({ test: 1 });
+
+  // this method will be highlighted as invalid,
+  // with @types/node automatically catching it as non-existent in `res`:
+  // res.fail({ test: 1 });
+});
 
 app.listen(PORT, () =>
   console.log(`Express.js server running on port: ${PORT}`),
